3a46ad8441b915cb3c65c556bb3eb7676ced5243
Sumon <mdsumonshik1@gmail.com>
2024-02-04 19:41:53 +0600

    SingUp.xml SingUpActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.notebook"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.notebook"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
