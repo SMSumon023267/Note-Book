191aff4b3e18c2dd303c992f44b210e28dd243a0
Sumon <mdsumonshik1@gmail.com>
2024-02-06 22:42:44 +0600

    click newUserSingInButton got to MainActivity.this,singup.class

--- app/src/main/java/com/example/notebook/MainActivity.java
+++ app/src/main/java/com/example/notebook/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.notebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText emailloginEditText,passwordloginEditText;
-    private Button loginButton,singUpButton;
+    private Button loginButton,newUserSingInButton;
 
     private TextView forgetPasswordTextView;
 
@@ -20,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
         emailloginEditText = (EditText) findViewById(R.id.emailloginMailActivity_EditText_ID);
         passwordloginEditText = (EditText) findViewById(R.id.passwordloginMailActivity_EditText_ID);
         loginButton = (Button) findViewById(R.id.loginButton_ID);
-        singUpButton = (Button) findViewById(R.id.singUpButton_ID);
+        newUserSingInButton = (Button) findViewById(R.id.newUserSingIn_Button_ID);
         forgetPasswordTextView = (TextView) findViewById(R.id.forgetPassword_TextView_ID);
+
+        newUserSingInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,singup.class));
+            }
+        });
     }
 }
