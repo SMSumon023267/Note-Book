999849ee3048f46d3fd770769a6010ce644317f0
Sumon <mdsumonshik1@gmail.com>
2024-02-06 22:36:46 +0600

    find EditText,Button,TextView MainActivity.java

--- app/src/main/java/com/example/notebook/MainActivity.java
+++ app/src/main/java/com/example/notebook/MainActivity.java
@@ -3,16 +3,24 @@ package com.example.notebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText emailloginEditText,passwordloginEditText;
+    private Button loginButton,singUpButton;
+
     private TextView forgetPasswordTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        emailloginEditText = (EditText) findViewById(R.id.emailloginMailActivity_EditText_ID);
+        passwordloginEditText = (EditText) findViewById(R.id.passwordloginMailActivity_EditText_ID);
+        loginButton = (Button) findViewById(R.id.loginButton_ID);
+        singUpButton = (Button) findViewById(R.id.singUpButton_ID);
+        forgetPasswordTextView = (TextView) findViewById(R.id.forgetPassword_TextView_ID);
     }
 }
