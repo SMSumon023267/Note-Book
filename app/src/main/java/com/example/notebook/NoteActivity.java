92912d26964d6c32f7cc0ffdd2a932653fd52a29
Sumon <mdsumonshik1@gmail.com>
2024-02-10 00:57:37 +0600

    FloatingButton Click go to CreateNoteActivity NoteActivity.java

--- app/src/main/java/com/example/notebook/NoteActivity.java
+++ app/src/main/java/com/example/notebook/NoteActivity.java
@@ -2,13 +2,28 @@ package com.example.notebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NoteActivity extends AppCompatActivity {
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingButton_ID);
+
+        
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NoteActivity.this,CreateNoteActivity.class));
+            }
+        });
     }
 }
