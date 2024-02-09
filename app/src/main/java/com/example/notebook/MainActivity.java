9f11112f6a8f1fdc9f722e063c98c7d828d8a381
Sumon <mdsumonshik1@gmail.com>
2024-02-10 00:30:58 +0600

    Login and Email verify MainActivity.java

--- app/src/main/java/com/example/notebook/MainActivity.java
+++ app/src/main/java/com/example/notebook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.notebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private EditText emailloginEditText,passwordloginEditText;
@@ -42,5 +48,45 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,ForgetPassword.class));
             }
         });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emaillogin = emailloginEditText.getText().toString().trim();
+                String passwordlogin = passwordloginEditText.getText().toString().trim();
+                if (emaillogin.isEmpty()){
+                    emailloginEditText.setError("Please Enter Your Email");
+                    emailloginEditText.requestFocus();
+                    return;
+                } else if (passwordlogin.isEmpty()) {
+                    passwordloginEditText.setError("Enter Password");
+                    passwordloginEditText.requestFocus();
+                    return;
+                }else {
+                    firebaseAuth.signInWithEmailAndPassword(emaillogin,passwordlogin).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                chackEmailVerification();
+                            }else {
+                                Toast.makeText(MainActivity.this, "Account Doesn't Exist", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+                }
+            }
+        });
+    }
+    private void chackEmailVerification(){
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser.isEmailVerified()==true){
+            Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(MainActivity.this,NoteActivity.class));
+        }else {
+            Toast.makeText(MainActivity.this, "Verify your email fast", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
