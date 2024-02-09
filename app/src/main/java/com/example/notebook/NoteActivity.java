1beeae41ffe1225790f8bdd5c59e127a10e17983
Sumon <mdsumonshik1@gmail.com>
2024-02-10 01:15:53 +0600

    menu Click Logout NoteActivity.java

--- app/src/main/java/com/example/notebook/NoteActivity.java
+++ app/src/main/java/com/example/notebook/NoteActivity.java
@@ -1,29 +1,52 @@
 package com.example.notebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NoteActivity extends AppCompatActivity {
     private FloatingActionButton floatingActionButton;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
+        firebaseAuth = FirebaseAuth.getInstance();
         floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingButton_ID);
 
-        
+
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(NoteActivity.this,CreateNoteActivity.class));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logOut_Menu_ID){
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(NoteActivity.this,MainActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
