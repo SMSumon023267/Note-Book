c4b243a15809aff50dce4d7cbc677e1bf21ea087
Sumon <mdsumonshik1@gmail.com>
2024-02-19 01:34:00 +0600

    Firestore Theke Data show firebasemodel.java NoteActivity.java

--- app/src/main/java/com/example/notebook/NoteActivity.java
+++ app/src/main/java/com/example/notebook/NoteActivity.java
@@ -2,19 +2,44 @@ package com.example.notebook;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class NoteActivity extends AppCompatActivity {
     private FloatingActionButton floatingActionButton;
     private FirebaseAuth firebaseAuth;
+
+
+    RecyclerView recyclerView;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+    FirestoreRecyclerAdapter<firebasemodel,NoteViewHolder> noteAdapter;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +49,10 @@ public class NoteActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingButton_ID);
 
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +60,57 @@ public class NoteActivity extends AppCompatActivity {
                 startActivity(new Intent(NoteActivity.this,CreateNoteActivity.class));
             }
         });
+
+        Query query = firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("MyNotes")
+                .orderBy("title",Query.Direction.ASCENDING);
+
+        FirestoreRecyclerOptions<firebasemodel> allUserNotes = new FirestoreRecyclerOptions.Builder<firebasemodel>()
+                .setQuery(query,firebasemodel.class).build();
+
+        noteAdapter = new FirestoreRecyclerAdapter<firebasemodel, NoteViewHolder>(allUserNotes) {
+            @Override
+            protected void onBindViewHolder(@NonNull NoteViewHolder holder, int position, @NonNull firebasemodel model) {
+                holder.noteTitle.setText(model.getTitle());
+                holder.noteContent.setText(model.getContent());
+
+
+            }
+
+            @NonNull
+            @Override
+            public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_layout,parent,false);
+                return new NoteViewHolder(view);
+
+            }
+        };
+
+        recyclerView = findViewById(R.id.recyclerView_ID);
+        recyclerView.setHasFixedSize(true);
+        staggeredGridLayoutManager =new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+
+        recyclerView.setAdapter(noteAdapter);
+
+
     }
+
+    private class NoteViewHolder extends RecyclerView.ViewHolder{
+        private TextView noteTitle;
+        private TextView noteContent;
+
+        LinearLayout linearLayoutNote;
+
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            noteTitle = itemView.findViewById(R.id.noteTitle_TextView_ID);
+            noteContent = itemView.findViewById(R.id.noteContent_TextView_ID);
+            linearLayoutNote = itemView.findViewById(R.id.linearLayoutNoteLayout_ID);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +127,19 @@ public class NoteActivity extends AppCompatActivity {
             startActivity(new Intent(NoteActivity.this,MainActivity.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (noteAdapter!=null){
+            noteAdapter.startListening();
+        }
     }
 }
